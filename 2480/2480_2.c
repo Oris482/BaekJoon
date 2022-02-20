1b4a7af77611aa6d47a5d40dd895cef2c0e5e162
jaesjeon <jaesjeon@student.42seoul.kr>
2022-02-21 00:57:33 +0900

    2480 alter_solve

---
+++ 2480/2480_2.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+
+static int set_flag(char *dice_flag, int *dice)
+{
+    int     idx = -1;
+    int     is_same_or_big = 0;
+    int     max_dice = 0;
+
+    *dice_flag = 0;
+    while (++idx < 3)
+    {
+        if (max_dice < dice[idx])
+            max_dice = dice[idx];
+        if ((*dice_flag & (1 << (dice[idx] - 1))) != 0)
+            is_same_or_big = dice[idx];
+        else
+            *dice_flag |= (1 << (dice[idx] - 1));
+    }
+    if (is_same_or_big == 0)
+        return (max_dice * (-1));
+    return (is_same_or_big);
+}
+
+int main(void)
+{
+    int     dice[3];
+    char    dice_flag;
+    int     same_dice;
+    int     count = 0;
+    int     idx = -1;
+
+    scanf("%d %d %d", &dice[0], &dice[1], &dice[2]);
+    while (++idx < 3)
+    {
+        if (dice[idx] < 1 || dice[idx] > 6)
+            return (0);
+    }
+    same_dice = set_flag(&dice_flag, dice);
+    if (same_dice < 0)
+    {
+        printf("%d", (same_dice * 100 * (-1)));
+        return (0);
+    }
+    idx = 1;
+    while (idx++ <= 6)
+    {
+        if ((dice_flag & 1) == 1)
+            count++;
+        dice_flag >>= 1;   
+    }
+    if (count == 2)
+        printf("%d", (same_dice * 100 + 1000));
+    else
+        printf("%d", (same_dice * 1000 + 10000));
+    return (0);
+}
